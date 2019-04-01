248a7ab577e5e59324fde609af63ce391d216035
steff456 <steff456@hotmail.com>
2019-04-01 11:17:13 -0500

    checkpoint

--- src/ngsep/genome/Transposon.java
+++ src/ngsep/genome/Transposon.java
@@ -2,6 +2,7 @@ package ngsep.genome;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import ngsep.alignments.ReadAlignment;
@@ -14,6 +15,15 @@ import ngsep.sequences.QualifiedSequence;
 
 public class Transposon {
 	
+	public class Tuple<X, Y> { 
+		  public final X x; 
+		  public final Y y; 
+		  public Tuple(X x, Y y) { 
+		    this.x = x; 
+		    this.y = y; 
+		  } 
+	}
+		
 	private Distribution distrHits = new Distribution(0, 100, 1);
 	
 	private ReferenceGenome genome;
@@ -23,6 +33,10 @@ public class Transposon {
 	private DefaultKmersMapImpl kmersMap;
 	
 	private HashMap<String, List<GenomicRegionImpl>> candidates;
+	
+	private List<Tuple<GenomicRegionImpl, GenomicRegionImpl>> ltr;
+	
+	private int count = 0;
 		
 	public void run() {
 		// Kmers - Subsequence
@@ -34,7 +48,26 @@ public class Transposon {
 		}
 		//TODO: process tandem repeats
 		//TODO: Find candidates that are nearer than 5kb
+		findNearCandidates();
 		//TODO: Find LTR form
+	}
+	
+	public void findNearCandidates() {
+		for(String candidate: candidates.keySet()) {
+			List<GenomicRegionImpl> genomicRegions = candidates.get(candidate);
+			// Compare the distance among the genomic regions
+			for (int i = 0; i < genomicRegions.size(); i++) {
+				GenomicRegionImpl actRegion = genomicRegions.get(i);
+				for (int j = i+1; j < genomicRegions.size(); j++) {
+					GenomicRegionImpl secRegion = genomicRegions.get(j);
+					int distance = GenomicRegionPositionComparator.getInstance().compare(actRegion, secRegion);
+					if (distance >= 5000) {
+						ltr.add(new Tuple<GenomicRegionImpl, GenomicRegionImpl>(actRegion, secRegion));
+					}
+				}
+			}
+			
+		}
 	}
 	
 	public void printResults() {
@@ -49,7 +82,7 @@ public class Transposon {
 			CharSequence kmer = seq.subSequence(i, (i+lengthKmer));
 			List<ReadAlignment> hits = fm.search(kmer.toString());
 			distrHits.processDatapoint(hits.size());
-			if(hits.size() > 100 ) {
+			if(hits.size() > 10 ) {
 				// If the kmer is more than x times
 				kmersMap.addOcurrance(kmer);
 				GenomicRegionImpl temp = new GenomicRegionImpl(kmer.toString(), i, i+lengthKmer);
@@ -63,6 +96,7 @@ public class Transposon {
 					n.add(temp);
 					candidates.put(kmer.toString(), n);
 				}
+				count++;
 			}
 		}
 	}
@@ -73,7 +107,6 @@ public class Transposon {
 		instance.genome = new ReferenceGenome(args[0]);
 		// Load short tandem repeats from the .fa file
 		//TODO: Write the loader for short tandem repeats
-		
 		// FM Index
 		instance.fm = new ReferenceGenomeFMIndex(instance.genome);
 		
@@ -81,8 +114,12 @@ public class Transposon {
 		
 		instance.candidates = new HashMap();
 		
+		instance.ltr = new ArrayList<>();
+		
 		instance.run();
 		instance.printResults();
 		System.out.println(instance.kmersMap.size());
+		System.out.println(instance.count);
+		System.out.println(instance.ltr.size());
 	}
 }
