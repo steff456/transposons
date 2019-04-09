ca3c16c6d0d55aa11ff9371136f9a39517635e94
steff456 <steff456@hotmail.com>
2019-04-09 15:30:39 -0500

    fixes in reading and start comparing

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 """Evaluation code for LTR."""
 from models.transposon import Transposon
 from utils.arguments import get_args
+import pdb
 
 
 def process_file(filename, mode='gt'):
     """Process the groundtruth/predictions file."""
     print('Processing {} as groundtruth'.format(filename))
     f = open(filename, 'r')
-    LTRs = []
+    LTRs = {}
+    act_name = ''
     for line in f.readlines():
         if mode == 'gt':
             if 'long_terminal_repeat' in line or 'LTR' in line:
                 data = line.split('\t')
-                LTRs.append(Transposon(data[0], data[1], data[2]))
+                if act_name != data[0]:
+                    act_LTRs = []
+                    act_name = data[0]
+                    act_LTRs.append(Transposon(data[0], data[1], data[2]))
+                    LTRs[act_name] = act_LTRs
+                else:
+                    act_LTRs[act_name].append(Transposon(data[0],
+                                                         data[1], data[2]))
         else:
             data = line.split('\t')
             LTRs.append(Transposon(data[0], data[1], data[2]))
@@ -22,6 +31,10 @@ def process_file(filename, mode='gt'):
 
 def compare(gt, pred):
     """Find overlap in the gt and predicted sites."""
+    for seq_name in gt.keys():
+        act_gt = gt[seq_name]
+        act_pred = pred[seq_name]
+        pdb.set_trace()
 
 
 def main():
